384d9594d0262454bdce968d366e89c0c0431d55
Louis Conlon <conlongo2@gmail.com>
2023-05-08 15:00:49 +0200

    CLEANED

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <button type="button" @click="count++">you have {{ count }} bones</button>
+  </div>
+  CODENOSIS
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
