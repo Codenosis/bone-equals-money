8f21dfd922f88985885bd06beb5d4351324d3b2c
Louis Conlon <conlongo2@gmail.com>
2023-05-08 13:31:33 +0200

    test push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,10 +18,7 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    wew this is so cool and stuff
   </p>
   <p>
     Install
